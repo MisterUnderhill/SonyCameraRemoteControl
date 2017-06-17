138bd97c6009b26cc6be757e3ea3cd95c2b09424
MisterUnderhill <simon.hobbins@hotmail.co.uk>
2017-06-17 20:10:58 +0100

    Removed "Hello World" Used Toast messages to report on the changes to a simple slider widget

--- app/src/main/java/uk/co/lost_boyproductions/sonycameraremotecontrol/MainActivity.java
+++ app/src/main/java/uk/co/lost_boyproductions/sonycameraremotecontrol/MainActivity.java
@@ -3,14 +3,51 @@ package uk.co.lost_boyproductions.sonycameraremotecontrol;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+    SeekBar lensZoom;
+    int lensZoomPosition = 50;
+    int lensZoomOldPosition = 50; // set to the same value as android.progress in layout definition
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView helloTextView = (TextView) findViewById(R.id.text_view_id);
-        helloTextView.setText(R.string.hello_world);
+
+//        final TextView helloTextView = (TextView) findViewById(R.id.text_view_id);
+//        helloTextView.setText(R.string.hello_world);
+
+//        lensZoom.setMax(100);     // crashes app
+//        lensZoom.setMin(0);       // crashes app
+
+        lensZoom=(SeekBar)findViewById(R.id.lensZoom);
+        lensZoom.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+/*        if (progress > lensZoomPosition) { // this would be a zoom out action
+            Toast.makeText(getApplicationContext(), "Increased", Toast.LENGTH_SHORT).show();
+        } else { // this would be a zoom in action
+            Toast.makeText(getApplicationContext(), "Decreased", Toast.LENGTH_SHORT).show();
+        }
+*/    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+//        Toast.makeText(getApplicationContext(),"seekbar touch started!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        lensZoomPosition = lensZoom.getProgress(); //
+        if (lensZoomOldPosition < lensZoomPosition) {
+            Toast.makeText(getApplicationContext(),"seekbar final position is higher: " + lensZoomPosition, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(),"seekbar final position is lower: " + lensZoomPosition, Toast.LENGTH_SHORT).show();
+        }
+        lensZoomOldPosition = lensZoomPosition;
+//        Toast.makeText(getApplicationContext(),"seekbar touch stopped!", Toast.LENGTH_SHORT).show();
     }
 }
